4e4f18b9ab3699602e5655b86e4912f495ad776d
Elvis Lumnica <58716184+elumnica@users.noreply.github.com>
2020-08-10 21:52:08 -0400

    optimal solution

--- quickSort.js
+++ quickSort.js
@@ -12,5 +12,25 @@ function quickSortHelper(array, startIdx, endIdx){
     let leftIdx = startIdx + 1;
     let rightIdx = endIdx;
     while (rightIdx >= leftIdx){
+        if (array[leftIdx] > array[pivotIdx] && array[rightIdx] < array[pivotIdx]){
+            swap(leftIdx, rightIdx, array
+        }
+        if (array[leftIdx] <= array[pivotIdx]) leftIdx++;
+        if (array[rightIdx] >= array[pivotIdx]) rightIdx--;
     }
+    swap(pivotIdx, rightIdx, array);
+    const leftSubarrayIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1);
+    if (leftSubarrayIsSmaller){
+        quickSortHelper(array, startIdx, rightIdx - 1);
+        quickSortHelper(array, rightIdx + 1, endIdx);
+    } else{
+        quickSortHelper(array, rightIdx + 1, endIdx);
+        quickSortHelper(array, startIdx, rightIdx - 1);
+    }
+}
+
+function swap(i, j, array){
+    let temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
 }
